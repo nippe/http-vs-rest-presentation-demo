00719e8a74301a639eb337641f6c5b5b6d8f849e
Niklas Nihlén <niklas.nihlen@danielwellington.com>
2021-03-25 22:15:42 +0100

    Major remodelling in folder structure

--- http-api-implicit/.aws-sam/build/HelloWorldFunction/app.js
+++ http-api-implicit/.aws-sam/build/HelloWorldFunction/app.js
@@ -15,19 +15,11 @@ let response;
  *
  */
 exports.lambdaHandler = async (event, context) => {
-  try {
-    // const ret = await axios(url);
-    response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world from RestApi',
-        // location: ret.data.trim()
-      }),
-    };
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-
-  return response;
+  console.log(event);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "hello world from HttpApi",
+    }),
+  };
 };
---
+++ http-api-implicit/events/logged-event-http.js
@@ -0,0 +1,49 @@
+const evt = {
+  version: "2.0",
+  routeKey: "GET /hello",
+  rawPath: "/hello",
+  rawQueryString: "q=niklas",
+  headers: {
+    accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,sv;q=0.7",
+    "content-length": "0",
+    host: "k4djwj7qhh.execute-api.eu-west-1.amazonaws.com",
+    "sec-ch-ua":
+      '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+    "x-amzn-trace-id": "Root=1-605cf83a-6f73c69507d34f291158fdf1",
+    "x-forwarded-for": "37.123.157.2",
+    "x-forwarded-port": "443",
+    "x-forwarded-proto": "https",
+  },
+  queryStringParameters: { q: "niklas" },
+  requestContext: {
+    accountId: "618713795450",
+    apiId: "k4djwj7qhh",
+    domainName: "k4djwj7qhh.execute-api.eu-west-1.amazonaws.com",
+    domainPrefix: "k4djwj7qhh",
+    http: {
+      method: "GET",
+      path: "/hello",
+      protocol: "HTTP/1.1",
+      sourceIp: "37.123.157.2",
+      userAgent:
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+    },
+    requestId: "cwu5Njm3joEEP7Q=",
+    routeKey: "GET /hello",
+    stage: "$default",
+    time: "25/Mar/2021:20:53:14 +0000",
+    timeEpoch: 1616705594826,
+  },
+  isBase64Encoded: false,
+};
